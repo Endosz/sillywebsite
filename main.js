519fca22ad64efa921252ba244a51d1acf40de5e
Endosz <154355428+Endosz@users.noreply.github.com>
2024-01-02 15:52:10 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-
+let currentShitters= []
+let exShitters = []
+currentShitters.push("Hutmuncher","NecroStickIsReal","Ducky_Jar")
+exShitters.push("Endosz")
+function printCurrentShitters(values){
+document.getElementById("current").innerHTML+= `<li> ${values} </li>`
+}
+currentShitters.forEach(printCurrentShitters)
+function printExShitters(values){
+document.getElementById("ex").innerHTML+= `<li> ${values} </li>`
+}
+exShitters.forEach(printExShitters)
